451a82402019a82d988d7866b924672cb0a177c2
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-07 02:47:56 +0800

    adawdasdas

--- web/src/app/core/payment/payment.component.ts
+++ web/src/app/core/payment/payment.component.ts
@@ -1,42 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import swal from "sweetalert2";
 
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  selector: "app-payment",
+  templateUrl: "./payment.component.html",
+  styleUrls: ["./payment.component.scss"],
 })
 export class PaymentComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(
-    private router: Router
-  ) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   navigatePage(path: string) {
     // console.log('Path: ', path)
-    this.router.navigate([path])
+    this.router.navigate([path]);
   }
 
   makePayment() {
-    swal.fire({
-      title: 'Success',
-      text: 'Payment successfully been made!',
-      icon: 'success',
-      buttonsStyling: false,
-      confirmButtonText: 'Okay',
-      customClass: {
-        confirmButton: 'btn btn-success '
-      }
-    }).then((result) => {
-      if (result.value) {
-        this.router.navigate(['/profile'])
-      }
-    })
-    console.log('confirm')
+    swal
+      .fire({
+        title: "Success",
+        text: "Payment successfully been made!",
+        icon: "success",
+        buttonsStyling: false,
+        confirmButtonText: "Okay",
+        customClass: {
+          confirmButton: "btn btn-success ",
+        },
+      })
+      .then((result) => {
+        if (result.value) {
+          this.router.navigate(["/orders"]);
+        }
+      });
+    console.log("confirm");
   }
-
 }
