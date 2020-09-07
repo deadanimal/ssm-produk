41bc6cccbf8b770bf9c96717d5858d914edbe4f4
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-07 08:13:52 +0800

    change slider banner

--- web/src/app/core/search-engine/search-engine.component.ts
+++ web/src/app/core/search-engine/search-engine.component.ts
@@ -1,55 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
-import { ProductsService } from 'src/app/shared/services/products/products.service';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ProductsService } from "src/app/shared/services/products/products.service";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
-  selector: 'app-search-engine',
-  templateUrl: './search-engine.component.html',
-  styleUrls: ['./search-engine.component.scss']
+  selector: "app-search-engine",
+  templateUrl: "./search-engine.component.html",
+  styleUrls: ["./search-engine.component.scss"],
 })
 export class SearchEngineComponent implements OnInit {
-
   // Search field
-  focus
-  searchField: string = ''
+  focus;
+  searchField: string = "";
 
   // Checker
-  isEmpty = true
-  isNoResult = false
-  isGotResult = false
+  isEmpty = true;
+  isNoResult = false;
+  isGotResult = false;
 
   // Form
-  productForm: FormGroup
+  productForm: FormGroup;
 
   // Data
-  searchResults: any[] = []
-  pdfProduct: any
+  searchResults: any[] = [];
+  pdfProduct: any;
+
+  // slider
+  slider1 = "assets/img/banner/banner portal-01.png";
+  slider2 = "assets/img/banner/banner portal-02.png";
+  slider3 = "assets/img/banner/banner portal-03.png";
+  slider4 = "assets/img/banner/banner portal-04.png";
 
   constructor(
     private router: Router,
     private fb: FormBuilder,
     private productService: ProductsService,
     private spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.productForm = this.fb.group({
-      name: new FormControl('getCompProfile'),
-      registration_number: new FormControl('', Validators.compose([
-        Validators.required
-      ]))
-    })
+      name: new FormControl("getCompProfile"),
+      registration_number: new FormControl(
+        "",
+        Validators.compose([Validators.required])
+      ),
+    });
   }
 
   navigatePage(path: string) {
     // console.log('Path: ', path)
-    this.router.navigate([path])
+    this.router.navigate([path]);
   }
 
   search() {
-    
     // if (this.searchField == '') {
 
     // }
@@ -61,25 +71,25 @@ export class SearchEngineComponent implements OnInit {
     //   }
     // )
     this.spinner.show();
-    console.log(this.productForm.value.registration_number)
-    setTimeout(
-      () => {
-        this.spinner.hide()
-        if (this.productForm.value.registration_number == 'Pipeline Network' || this.productForm.value.registration_number == '960536') {
-          this.isGotResult = true
-          this.isEmpty = false
-        }
-        else {
-          this.isGotResult = true
-          this.isEmpty = false
-        }
-      }, 2000
-    )
+    console.log(this.productForm.value.registration_number);
+    setTimeout(() => {
+      this.spinner.hide();
+      if (
+        this.productForm.value.registration_number == "Pipeline Network" ||
+        this.productForm.value.registration_number == "960536"
+      ) {
+        this.isGotResult = true;
+        this.isEmpty = false;
+      } else {
+        this.isGotResult = true;
+        this.isEmpty = false;
+      }
+    }, 2000);
     // this.productService.search(this.productForm.value).subscribe(
     //   (res) => {
     //     this.spinner.hide();
     //     this.searchResults = this.productService.product
-    //     this.isEmpty = false 
+    //     this.isEmpty = false
     //     if (!res) {
     //       this.isNoResult = true
 
@@ -94,19 +104,18 @@ export class SearchEngineComponent implements OnInit {
     //   },
     //   () => {}
     // )
-    
   }
 
   viewResult() {
-    console.log('View result')
-    this.router.navigate(['/product-listing'])
+    console.log("View result");
+    this.router.navigate(["/product-listing"]);
   }
 
   getPDF() {
     let hello = {
-      test: this.searchResults
-    }
-    console.log(hello)
+      test: this.searchResults,
+    };
+    console.log(hello);
     this.spinner.show();
     this.productService.getPDF(hello).subscribe(
       (res) => {
@@ -117,7 +126,6 @@ export class SearchEngineComponent implements OnInit {
         this.spinner.hide();
       },
       () => {}
-    )
+    );
   }
-
 }
