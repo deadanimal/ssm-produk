218d83342e718d5787c6b91dc658d2f6527a07f7
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-09 16:41:06 +0800

    qewwqewqeq

--- web/src/app/core/cbid/cbid.component.ts
+++ web/src/app/core/cbid/cbid.component.ts
@@ -7,6 +7,29 @@ import { Router } from "@angular/router";
   styleUrls: ["./cbid.component.scss"],
 })
 export class CbidComponent implements OnInit {
+  clicked = false;
+
+  data: any = [
+    {
+      name: "Registration Of Businesses (ROB)",
+      type: "Statistics",
+      amount: "10",
+      created_at: "2019-07-27T01:07:14Z",
+    },
+    {
+      name: "Registration Of Company (ROC)",
+      type: "Listing",
+      amount: "20",
+      created_at: "2019-07-27T01:07:14Z",
+    },
+    {
+      name: "Both",
+      type: "Both",
+      amount: "30",
+      created_at: "2019-07-27T01:07:14Z",
+    },
+  ];
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
@@ -14,5 +37,9 @@ export class CbidComponent implements OnInit {
   navigatePage(path: string) {
     // console.log('Path: ', path)
     this.router.navigate([path]);
+  }
+
+  showSummary() {
+    this.clicked = true;
   }
 }
