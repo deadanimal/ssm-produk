a3ff50c6702bf55579a58cefe1481fe9c88c5aa8
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-09-11 07:12:50 +0800

    admin update

--- webapp/src/app/core/admin/product/resupply/resupply.component.ts
+++ webapp/src/app/core/admin/product/resupply/resupply.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResupplyComponent implements OnInit {
 
+  // Image 
+  imgConstruction = 'assets/img/default/under-construction.png'
+  
   constructor() { }
 
   ngOnInit() {
