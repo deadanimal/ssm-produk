c5f8a2a2b9065eff0755cdfea427a96ef16348a8
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-09-14 11:23:21 +0800

    piupiu

---
+++ api/transactions/serializers.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from calendar import timegm
+import json
+
+from django.contrib.auth.forms import PasswordResetForm
+from django.conf import settings
+from django.utils.translation import gettext as _
+from rest_framework import serializers
+from django.utils.timezone import now
+
+from .models import (
+    Transaction,
+    CartCBID,
+    Reconcile
+)
+
+class TransactionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+
+class CartCBIDSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CartCBID
+        fields = '__all__'
+
+
+class ReconcileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reconcile
+        fields = '__all__'
+
