23f7efa96b7ed0f4b28c9e468acf5bdfdc50dab9
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-09-17 05:54:34 +0800

    update api

---
+++ script/import_roc.py
@@ -0,0 +1,31 @@
+# import csv
+import requests
+import json
+# Reading an excel file using Python 
+import xlrd
+import csv
+
+
+with open('data/roc-v1.1.txt', mode='r') as txt_file:
+    txt_reader = csv.reader(txt_file, delimiter='|')
+    fields = [next(txt_reader)]
+    base_url = 'https://ssm-product-api.pipe.my/'
+    index_counter = 0   
+
+    for row in txt_reader:
+
+        # print(row[2])
+        index_counter = index_counter + 1
+        company = {
+            'name': row[0],
+            'company_number': row[1],
+            'company_number_new': row[3],
+            'check_digit': row[2],
+            'type_of_entity': 'CP'
+        }
+        # print(company)
+
+        r_reg = requests.post(base_url + 'v1/outfits/', data=company)
+        print(index_counter, r_reg.status_code)
+        # res_reg = r_reg.json()
+
