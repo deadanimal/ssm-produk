396cccfc0f8991b22972b6448fcbd2ace95ba306
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2020-09-22 14:57:27 +0800

    asdda dsdaaxa dsdada

---
+++ web/src/app/core/enquiry-history/enquiry-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enquiry-history',
+  templateUrl: './enquiry-history.component.html',
+  styleUrls: ['./enquiry-history.component.scss']
+})
+export class EnquiryHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
