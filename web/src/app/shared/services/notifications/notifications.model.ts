cf6c2e30da69cd4f70187e0607179434f05612d7
Afeez Aziz <afeezaziz@gmail.com>
2020-10-01 18:52:25 +0800

    lol

---
+++ web/src/app/shared/services/notifications/notifications.model.ts
@@ -0,0 +1,15 @@
+export class Notification {
+    public id: string;
+    public name: string;
+    public notificationDate: string;
+
+    constructor(
+        id: string,
+        name: string,
+        notificationDate: string,
+    ) {
+        this.id = id.
+        this.name = name;
+        this.notificationDate = notificationDate;
+    }
+}
