196c1020dfef200171751836cfdc6760dedb705f
Afeez Aziz <afeezaziz@gmail.com>
2020-10-03 22:09:00 +0800

    afeez habis api sikit

--- api-2/services/models.py
+++ api-2/services/models.py
@@ -25,13 +25,57 @@ class Service(models.Model):
         ('NA', 'Not Available')
     ]
     
-    service_type = models.CharField(choices=SERVICE_TYPE, max_length=2, default='PD')    
-    remarks = models.TextField(null=True)
-    completed = models.BooleanField(null=True)
+    service_type = models.CharField(choices=SERVICE_TYPE, max_length=2, default='NA')    
+
+    ENTITIES_TYPE = [
+        ('RB', 'Registration of Business'),
+        ('RC', 'Registration of Company'),
+
+        ('NA', 'Not Available')
+    ]
+
+    fee = models.IntegerField(default=1000)
+    
+    entities_type = models.CharField(choices=ENTITIES_TYPE, max_length=2, default='NA')   
+
+    PRODUCT_TYPE = [
+        ('PR', 'Product'),
+        ('LS', 'Listing'),
+
+        ('NA', 'Not Available')
+    ]
+    
+    product_type = models.CharField(choices=PRODUCT_TYPE, max_length=2, default='NA')           
 
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
     class meta:
         ordering = ['created_date']
+
+class ServiceRequest(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)        
+
+    name = models.CharField(max_length=512, null=True)
+    organisation = models.CharField(max_length=512, null=True) 
+    address = models.CharField(max_length=512, null=True) 
+    # address1 = models.CharField(max_length=512, null=True) 
+    # address2 = models.CharField(max_length=512, null=True) 
+    # address3 = models.CharField(max_length=512, null=True) 
+    # postcode = models.CharField(max_length=512, null=True) 
+    # country = models.CharField(max_length=512, null=True) 
+    # city = models.CharField(max_length=512, null=True) 
+    email_address = models.CharField(max_length=512, null=True) 
+    phone_number = models.CharField(max_length=512, null=True) 
+
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, null=True)
+
+    remarks = models.TextField(null=True)
+    completed = models.BooleanField(default=False)
  
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class meta:
+        ordering = ['created_date']
