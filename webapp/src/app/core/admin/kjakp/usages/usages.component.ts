a722f37e311f5939161e60e2fc58091460fffe83
Afeez Aziz <afeezaziz@gmail.com>
2020-10-05 01:39:40 +0800

    dsa

--- webapp/src/app/core/admin/kjakp/usages/usages.component.ts
+++ webapp/src/app/core/admin/kjakp/usages/usages.component.ts
@@ -1,4 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  NgZone,
+  TemplateRef,
+} from "@angular/core";
+import { User } from "src/assets/mock/admin-user/users.model";
+import { MocksService } from "src/app/shared/services/mocks/mocks.service";
+
+import * as moment from "moment";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import { BsModalRef, BsModalService } from "ngx-bootstrap";
+am4core.useTheme(am4themes_animated);
+
+import swal from "sweetalert2";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ServicesService } from 'src/app/shared/services/services/services.service';
+
+export enum SelectionType {
+  single = "single",
+  multi = "multi",
+  multiClick = "multiClick",
+  cell = "cell",
+  checkbox = "checkbox",
+}
+
 
 @Component({
   selector: 'app-usages',
@@ -6,13 +39,150 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usages.component.scss']
 })
 export class UsagesComponent implements OnInit {
+  // Table
+  tableEntries: number = 10;
+  tableSelected: any[] = [];
+  tableTemp = [];
+  tableActiveRow: any;
+  tableRows: any[] = [];
+  SelectionType = SelectionType;
+  requestList: any;
 
-  // Image 
-  imgConstruction = 'assets/img/default/under-construction.png'
-  
-  constructor() { }
+  // Modal
+  modal: BsModalRef;
+  modalConfig = {
+    keyboard: true,
+    class: "modal-dialog-centered",
+  };
+
+  // Form
+  registerForm: FormGroup;
+  registerFormMessages = {
+    name: [{ type: "required", message: "Name is required" }],
+    email: [
+      { type: "required", message: "Email is required" },
+      { type: "email", message: "A valid email is required" },
+    ],
+  };
+
+  constructor(
+    private mockService: MocksService,
+    private modalService: BsModalService,
+    private formBuilder: FormBuilder,
+    private zone: NgZone,
+    private servicesService: ServicesService,
+  ) {
+    // this.getData()
+  }
 
   ngOnInit() {
+    // this.registerForm = this.formBuilder.group({
+    //   name: new FormControl("", Validators.compose([Validators.required])),
+    //   email: new FormControl(
+    //     "",
+    //     Validators.compose([Validators.required, Validators.email])
+    //   ),
+    // });
+
+    this.initData();
   }
+
+  ngOnDestroy() {
+    // this.zone.runOutsideAngular(() => {
+    //   if (this.chart) {
+    //     this.chart.dispose()
+    //   }
+    // })
+  }
+
+  getCharts() {
+    // this.zone.runOutsideAngular(() => {
+    //   this.getChart()
+    // })
+  }
+
+  openModal(modalRef: TemplateRef<any>) {
+    this.modal = this.modalService.show(modalRef, this.modalConfig);
+  }
+
+  closeModal() {
+    this.modal.hide();
+    this.registerForm.reset();
+  }
+
+  confirm() {
+    swal.fire({
+      title: "Success",
+      text: "You have successfully export report!",
+      type: "success",
+      buttonsStyling: false,
+      confirmButtonClass: "btn btn-success",
+      confirmButtonText: "Success",
+      showCancelButton: false,
+      // cancelButtonClass: "btn btn-danger",
+      // cancelButtonText: "Cancel"
+    });
+  }
+
+  register() {
+    swal
+      .fire({
+        title: "Success",
+        text: "A new user has been created!",
+        type: "success",
+        buttonsStyling: false,
+        confirmButtonClass: "btn btn-success",
+        confirmButtonText: "Close",
+      })
+      .then((result) => {
+        if (result.value) {
+          this.modal.hide();
+          this.registerForm.reset();
+        }
+      });
+  }
+
+  initData() {
+    this.servicesService.getReport().subscribe(
+      (res) => {
+        this.tableRows = res;
+        this.tableRows.forEach(
+          (row) => {
+            if(row.pending_date) {
+              row.pending_date = moment(row.pending_date).format('DD/MM/YYYY')
+            }
+
+            if(row.completed_date) {
+              row.completed_date = moment(row.completed_date).format('DD/MM/YYYY')
+            }
+
+            if(row.created_date) {
+              row.created_date = moment(row.created_date).format('DD/MM/YYYY')
+            }
+
+            if(row.modified_date) {
+              row.modified_date = moment(row.modified_date).format('DD/MM/YYYY')
+            }
+          }
+        )        
+      },
+      (err) => {
+
+      },
+      () => {
+        this.tableTemp = this.tableRows.map((prop, key) => {
+          return {
+            ...prop,
+            id_index: key+1
+          };
+        });        
+        console.log(this.tableTemp)
+      }
+    )
+  }
+
+  onActivate(event) {
+    this.tableActiveRow = event.row;
+  }  
 
 }
