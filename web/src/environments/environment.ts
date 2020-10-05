22c25ba2ff143de0f337a745bc4d3374829e35f2
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-10-06 07:50:24 +0800

    test

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://afeezaziz.ngrok.io/'
-  // baseUrl: 'https://ssm-product-api.pipe.my/',
+  // baseUrl: 'http://afeezaziz.ngrok.io/'
+  baseUrl: 'https://ssm-product-api.pipe.my/',
   // baseUrl: 'http://127.0.0.1:8000/'
 };
 
