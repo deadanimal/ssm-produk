e60929963b3bc6e02bdc4ad0ac4547793758c25b
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-10-14 15:10:45 +0800

    pushy

--- web/src/app/core/products/product-custom-data-package-b/product-custom-data-package-b.component.ts
+++ web/src/app/core/products/product-custom-data-package-b/product-custom-data-package-b.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import swal from "sweetalert2";
 
 @Component({
   selector: 'app-product-custom-data-package-b',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCustomDataPackageBComponent implements OnInit {
 
-  constructor() { }
+  // hide div summary request
+  clicked = false;
 
-  ngOnInit(): void {
+  // Form
+  entities = "rob";
+
+  // Search
+  focus;
+  searchField: string = "";
+
+  // Checker
+  isEmpty;
+  isNoResult = false;
+  isGotResult;
+
+  // Options
+  searchOpts = [
+    { text: "Audit Firm", value: "audit-firm" },
+    { text: "Business", value: "business" },
+    { text: "Company", value: "company" },
+  ];
+
+  data: any = [
+    {
+      idnum: "ROB",
+      entities: "123123123",
+      involvement: "Shareholder",
+      created_at: "2019-07-27T01:07:14Z",
+    },
+    {
+      idnum: "ROC",
+      entities: "2342342423",
+      involvement: "Business Ownership",
+      created_at: "2019-07-27T01:07:14Z",
+    },
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  navigatePage(path: string) {
+    // console.log('Path: ', path)
+    this.router.navigate([path]);
+  }
+
+  search() {
+    this.isNoResult = true;
+    // if (this.searchField == '') {
+
+    // }
+  }
+
+  showSummary() {
+    this.clicked = true;
+  }
+
+  confirm() {
+
   }
 
 }
