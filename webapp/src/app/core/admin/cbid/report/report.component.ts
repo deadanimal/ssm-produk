22b99d8258105e91175e558c8954f3e24f88c51f
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-10-14 13:46:41 +0800

    cbid

--- webapp/src/app/core/admin/cbid/report/report.component.ts
+++ webapp/src/app/core/admin/cbid/report/report.component.ts
@@ -1,28 +1,11 @@
-import {
-  Component,
-  OnInit,
-  OnDestroy,
-  NgZone,
-  TemplateRef,
-} from "@angular/core";
-import { User } from "src/assets/mock/admin-user/users.model";
-import { MocksService } from "src/app/shared/services/mocks/mocks.service";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import * as moment from "moment";
-import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { BsModalRef, BsModalService } from "ngx-bootstrap";
-am4core.useTheme(am4themes_animated);
+import { LoadingBarService } from '@ngx-loading-bar/core';
 
-import swal from "sweetalert2";
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  FormControl,
-} from "@angular/forms";
-import { ServicesService } from 'src/app/shared/services/services/services.service';
+import { ServicesService } from '../../../../shared/services/services/services.service';
+
+import * as moment from 'moment';
 
 export enum SelectionType {
   single = "single",
@@ -38,6 +21,7 @@ export enum SelectionType {
   styleUrls: ["./report.component.scss"],
 })
 export class CbidReportComponent implements OnInit {
+  
   // Table
   tableEntries: number = 10;
   tableSelected: any[] = [];
@@ -47,111 +31,31 @@ export class CbidReportComponent implements OnInit {
   SelectionType = SelectionType;
   requestList: any;
 
-  // Modal
-  modal: BsModalRef;
-  modalConfig = {
-    keyboard: true,
-    class: "modal-dialog-centered",
-  };
-
-  // Form
-  registerForm: FormGroup;
-  registerFormMessages = {
-    name: [{ type: "required", message: "Name is required" }],
-    email: [
-      { type: "required", message: "Email is required" },
-      { type: "email", message: "A valid email is required" },
-    ],
-  };
-
   constructor(
-    private mockService: MocksService,
-    private modalService: BsModalService,
-    private formBuilder: FormBuilder,
-    private zone: NgZone,
+    private loadingBar: LoadingBarService,
+    private router: Router,
     private servicesService: ServicesService,
   ) {
-    // this.getData()
+
   }
 
   ngOnInit() {
-    // this.registerForm = this.formBuilder.group({
-    //   name: new FormControl("", Validators.compose([Validators.required])),
-    //   email: new FormControl(
-    //     "",
-    //     Validators.compose([Validators.required, Validators.email])
-    //   ),
-    // });
-
-    this.initData();
-  }
-
-  ngOnDestroy() {
-    // this.zone.runOutsideAngular(() => {
-    //   if (this.chart) {
-    //     this.chart.dispose()
-    //   }
-    // })
-  }
-
-  getCharts() {
-    // this.zone.runOutsideAngular(() => {
-    //   this.getChart()
-    // })
-  }
-
-  openModal(modalRef: TemplateRef<any>) {
-    this.modal = this.modalService.show(modalRef, this.modalConfig);
-  }
-
-  closeModal() {
-    this.modal.hide();
-    this.registerForm.reset();
-  }
-
-  confirm() {
-    swal.fire({
-      title: "Success",
-      text: "You have successfully export report!",
-      type: "success",
-      buttonsStyling: false,
-      confirmButtonClass: "btn btn-success",
-      confirmButtonText: "Success",
-      showCancelButton: false,
-      // cancelButtonClass: "btn btn-danger",
-      // cancelButtonText: "Cancel"
-    });
-  }
-
-  register() {
-    swal
-      .fire({
-        title: "Success",
-        text: "A new user has been created!",
-        type: "success",
-        buttonsStyling: false,
-        confirmButtonClass: "btn btn-success",
-        confirmButtonText: "Close",
-      })
-      .then((result) => {
-        if (result.value) {
-          this.modal.hide();
-          this.registerForm.reset();
-        }
-      });
+    this.initData()
   }
 
   initData() {
-    this.servicesService.getReport().subscribe(
+    this.loadingBar.start()
+    this.servicesService.getAll().subscribe(
       (res) => {
         this.tableRows = res;
         this.tableRows.forEach(
           (row) => {
-            if(row.pending_date) {
-              row.pending_date = moment(row.pending_date).format('DD/MM/YYYY')
+            this.loadingBar.complete()
+            if(row.in_progress) {
+              row.in_progress_date = moment(row.in_progress_date).format('DD/MM/YYYY')
             }
 
-            if(row.completed_date) {
+            if(row.completed) {
               row.completed_date = moment(row.completed_date).format('DD/MM/YYYY')
             }
 
@@ -162,6 +66,9 @@ export class CbidReportComponent implements OnInit {
             if(row.modified_date) {
               row.modified_date = moment(row.modified_date).format('DD/MM/YYYY')
             }
+          },
+          () => {
+            this.loadingBar.complete()
           }
         )        
       },
--- webapp/src/app/shared/services/products/products.service.ts
+++ webapp/src/app/shared/services/products/products.service.ts
@@ -1,28 +1,41 @@
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { HttpClient } from "@angular/common/http";
-import { Form } from "@angular/forms";
-import { tap } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Form } from '@angular/forms';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class ProductsService {
 
   // URL
-  public productsURL: string = environment.baseUrl + "v1/products/";
+  public urlProducts: string = environment.baseUrl + 'v1/products/';
+
+  // Data
+  public product
+  public products: []
 
   constructor(private http: HttpClient) {}
 
-  getAllProducts(): any {
-    let url = this.productsURL;
-    return this.http.get(url).pipe(
+  getAll(): Observable<any[]> {
+    let url = this.urlProducts;
+    return this.http.get<any[]>(url).pipe(
       tap((res) => {
-        console.log("Products: ", res);
+        console.log('Products: ', res);
       })
     );    
+  }
+
+  patch(id: string, body: any): Observable<any> {
+    let urlTemp = this.urlProducts + id + '/'
+    return this.http.patch<any>(urlTemp, body).pipe(
+      tap((res) => {
+        console.log('Patched: ', res)
+      })
+    )
   }
 
 }
