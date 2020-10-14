93076ec9de742d84a950be05c3ef8c932b287484
Afeez Aziz <afeezaziz@gmail.com>
2020-10-14 13:40:18 +0800

    sa

--- api-2/products/helpers/change_name.py
+++ api-2/products/helpers/change_name.py
@@ -5,6 +5,8 @@ import json
 from datetime import datetime
 from django.utils.timezone import make_aware
 
+from products.helpers.mapping import comp_type_mapping, comp_status_mapping, comp_status_mapping, branch_code
+
 def change_name(mdw_1, mdw_2, lang):
     
     data_mdw_1 = mdw_1
@@ -13,11 +15,143 @@ def change_name(mdw_1, mdw_2, lang):
     date_format = "%d-%m-%Y"
     time_zone = 'Asia/Kuala_Lumpur'
 
+    incorp_date = data_mdw_1['incorpDate']
+    incorp_date = make_aware(datetime.strptime(incorp_date, '%Y-%m-%dT%H:%M:%S.000Z'))
+    incorp_date_str = incorp_date.astimezone(pytz.timezone(time_zone)).strftime(date_format)
+    
+    print(mdw_1)
+
+    if lang == 'ms':
+        if incorp_date.month == 1:
+            incorp_month = "Januari"
+        elif incorp_date.month == 2:
+            incorp_month = "Februari"
+        elif incorp_date.month == 3:
+            incorp_month = "Mac"            
+        elif incorp_date.month == 4:
+            incorp_month = "April"            
+        elif incorp_date.month == 5:
+            incorp_month = "Mei"            
+        elif incorp_date.month == 6:
+            incorp_month = "Jun"            
+        elif incorp_date.month == 7:
+            incorp_month = "Julai"            
+        elif incorp_date.month == 8:
+            incorp_month = "Ogos"            
+        elif incorp_date.month == 9:
+            incorp_month = "September"            
+        elif incorp_date.month == 10:
+            incorp_month = "Oktober"            
+        elif incorp_date.month == 11:
+            incorp_month = "November"            
+        elif incorp_date.month == 12:
+            incorp_month = "Disember"  
+    else:
+        if incorp_date.month == 1:
+            incorp_month = "January"
+        elif incorp_date.month == 2:
+            incorp_month = "February"
+        elif incorp_date.month == 3:
+            incorp_month = "March"            
+        elif incorp_date.month == 4:
+            incorp_month = "April"            
+        elif incorp_date.month == 5:
+            incorp_month = "May"            
+        elif incorp_date.month == 6:
+            incorp_month = "June"            
+        elif incorp_date.month == 7:
+            incorp_month = "July"            
+        elif incorp_date.month == 8:
+            incorp_month = "August"            
+        elif incorp_date.month == 9:
+            incorp_month = "September"            
+        elif incorp_date.month == 10:
+            incorp_month = "October"            
+        elif incorp_date.month == 11:
+            incorp_month = "November"            
+        elif incorp_date.month == 12:
+            incorp_month = "December"      
+
+    change_name_date = data_mdw_1['dateOfChange']
+    change_name_date = make_aware(datetime.strptime(change_name_date, '%Y-%m-%dT%H:%M:%S.000Z'))
+    change_name_date_str = change_name_date.astimezone(pytz.timezone(time_zone)).strftime(date_format)   
+
+    if lang == 'ms':
+        if change_name_date.month == 1:
+            change_name_month = "Januari"
+        elif change_name_date.month == 2:
+            change_name_month = "Februari"
+        elif change_name_date.month == 3:
+            change_name_month = "Mac"            
+        elif change_name_date.month == 4:
+            change_name_month = "April"            
+        elif change_name_date.month == 5:
+            change_name_month = "Mei"            
+        elif change_name_date.month == 6:
+            change_name_month = "Jun"            
+        elif change_name_date.month == 7:
+            change_name_month = "Julai"            
+        elif change_name_date.month == 8:
+            change_name_month = "Ogos"            
+        elif change_name_date.month == 9:
+            change_name_month = "September"            
+        elif change_name_date.month == 10:
+            change_name_month = "Oktober"            
+        elif change_name_date.month == 11:
+            change_name_month = "November"            
+        elif change_name_date.month == 12:
+            change_name_month = "Disember"  
+    else:
+        if change_name_date.month == 1:
+            change_name_month = "January"
+        elif change_name_date.month == 2:
+            change_name_month = "February"
+        elif change_name_date.month == 3:
+            change_name_month = "March"            
+        elif change_name_date.month == 4:
+            change_name_month = "April"            
+        elif change_name_date.month == 5:
+            change_name_month = "May"            
+        elif change_name_date.month == 6:
+            change_name_month = "June"            
+        elif change_name_date.month == 7:
+            change_name_month = "July"            
+        elif change_name_date.month == 8:
+            change_name_month = "August"            
+        elif change_name_date.month == 9:
+            change_name_month = "September"            
+        elif change_name_date.month == 10:
+            change_name_month = "October"            
+        elif change_name_date.month == 11:
+            change_name_month = "November"            
+        elif change_name_date.month == 12:
+            change_name_month = "December"                       
+
+    branch_name = branch_code(mdw_1['branchCode'])
+
+    act_year_enacted = datetime(year=2017,month=1, day=31).astimezone(pytz.timezone(time_zone))
+    
+    if incorp_date < act_year_enacted:
+        act_year = '1965'
+    else:
+        act_year = '2016'    
+
     data_ready = {
         'mdw1': mdw_1,
-        'mdw2': mdw_2
+        'mdw2': mdw_2,
+        'companyStatus': comp_status_mapping(mdw_1['companyStatus'],lang),
+        'companyType': comp_type_mapping(mdw_1['companyType'],lang),
+        'incorpDate': incorp_date_str,
+        'incorporate_day': incorp_date.day,
+        'incorporate_month': incorp_month,
+        'incorporate_year': incorp_date.year,
+        'change_name_date': change_name_date_str,
+        'change_name_day': change_name_date.day,
+        'change_name_month': change_name_month,
+        'change_name_year': incorp_date.year,        
+        'branch_name': branch_name,
+        'printing_time': datetime.now().astimezone(pytz.timezone(time_zone)).strftime("%d-%m-%Y %H:%M:%S"),
+        'act_year': act_year
     }
-
-    print(data_ready)
 
     return data_ready
--- api-2/products/helpers/particular_sharecapital.py
+++ api-2/products/helpers/particular_sharecapital.py
@@ -13,11 +13,65 @@ def particular_sharecapital(mdw_1, mdw_2, lang, entity_type):
     date_format = "%d-%m-%Y"
     time_zone = 'Asia/Kuala_Lumpur'
 
+
+    business_address_info = mdw_1["rocBusinessAddressInfo"]
+    registered_address_info = mdw_1["rocRegAddressInfo"]
+    share_capital_info = mdw_1["shareCapitalSummary"]
+    company_info = mdw_1["rocCompanyInfo"]
+
+    temp_comp_status_old = data_mdw_1["rocCompanyInfo"]['companyStatus']
+    
+    if temp_comp_status_old == 'E':
+        temp_comp_status_new = 'Existing'
+    elif temp_comp_status_old == 'W':
+        temp_comp_status_new = 'Winding Up'
+    elif temp_comp_status_old == 'D':
+        temp_comp_status_new = 'Dissolved'
+        
+
+    temp_incorpDate_old = make_aware(datetime.strptime(data_mdw_1["rocCompanyInfo"]['incorpDate'], '%Y-%m-%dT%H:%M:%S.000Z'))
+    temp_incorpDate_new = temp_incorpDate_old.astimezone(pytz.timezone(time_zone)).strftime(date_format)
+    
+    total_issued = float(mdw_1["shareCapitalSummary"]['totalIssued'])
+    #print(mdw_1["shareCapitalSummary"])
+    ordinary_issue_cash = float(mdw_1["shareCapitalSummary"]["ordinaryCash"])
+    ordinary_issue_noncash = float(mdw_1["shareCapitalSummary"]["ordinaryOtherwise"])
+    preference_issue_cash = float(mdw_1["shareCapitalSummary"]["preferenceCash"])
+    preference_issue_noncash = float(mdw_1["shareCapitalSummary"]["preferenceOtherwise"])
+    others_issue_cash = float(mdw_1["shareCapitalSummary"]["othersCash"])
+    others_issue_noncash = float(mdw_1["shareCapitalSummary"]["othersOtherwise"])
+
+    print(mdw_1)
+    print(mdw_1['allotmentOfShare'])
+    list_of_allotments = mdw_1['allotmentOfShare']['allotmentShareList']['allotmentShareList']
+
+    allotments_data = []
+    
+    for allotment in list_of_allotments:
+        allotment_data = allotment['dtAllot']['#text']
+        allotment_issued_share = allotment['issuedShare']
+        print(allotment)
+        print('\n')
+
+
+
+
     data_ready = {
         'mdw1': mdw_1,
-        'mdw2': mdw_2
+        'mdw2': mdw_2,
+        'business_address_info': business_address_info,
+        'registered_address_info': registered_address_info,
+        'share_capital_info': share_capital_info,
+        'total_issued': total_issued,
+        'ordinary_issue_cash': ordinary_issue_cash,
+        'ordinary_issue_noncash': ordinary_issue_noncash,
+        'preference_issue_cash': preference_issue_cash,
+        'preference_issue_noncash': preference_issue_noncash,
+        'others_issue_cash': others_issue_cash,
+        'others_issue_noncash': others_issue_noncash,
+        'incorp_date': temp_incorpDate_new,    
+        'company_info': company_info    
     }
 
-    print(data_ready)
 
     return data_ready
