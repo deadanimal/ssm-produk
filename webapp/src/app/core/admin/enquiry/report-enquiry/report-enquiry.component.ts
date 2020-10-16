d4dc653e24d1200b027ac0cd2b9104c9b8812933
Syafiq Basri <syafiqbasri@pipeline-network.com>
2020-10-16 21:11:46 +0800

    auch

---
+++ webapp/src/app/core/admin/enquiry/report-enquiry/report-enquiry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-enquiry',
+  templateUrl: './report-enquiry.component.html',
+  styleUrls: ['./report-enquiry.component.scss']
+})
+export class ReportEnquiryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
