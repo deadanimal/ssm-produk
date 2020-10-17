6df43ba8c31737d86cc3b4e126589c2ca6b838d4
Afeez Aziz <afeezaziz@gmail.com>
2020-10-17 16:52:09 +0800

    dsadsa

--- api-2/products/helpers/company_charges.py
+++ api-2/products/helpers/company_charges.py
@@ -7,17 +7,60 @@ from django.utils.timezone import make_aware
 
 def company_charges(mdw_1, mdw_2, lang, entity_type):
     
-    data_mdw_1 = mdw_1
-    data_mdw_2 = mdw_2
 
     date_format = "%d-%m-%Y"
     time_zone = 'Asia/Kuala_Lumpur'
 
+    charges = mdw_1["SSMRegistrationChargesInfos"]["SSMRegistrationChargesInfos"]
+    charges_list = []
+
+    if isinstance(charges, list): 
+        charges = charges
+    else:
+        charges = [charges]
+                
+
+    for charge in charges:
+        
+        if charge['chargeStatus'] == 'S':
+            charge['chargeStatusString'] = 'FULLY SATISFIED'
+        elif charge['chargeStatus'] == 'P':
+            charge['chargeStatusString'] = 'PARTLY SATISFIED'
+        elif charge['chargeStatus'] == 'R':
+            charge['chargeStatusString'] = 'FULLY RELEASED'  
+        elif charge['chargeStatus'] == 'Q':
+            charge['chargeStatusString'] = 'PARTLY RELEASED'     
+        elif charge['chargeStatus'] == 'U':
+            charge['chargeStatusString'] = 'UNSATISFIED'
+        elif charge['chargeStatus'] == 'B':
+            charge['chargeStatusString'] = 'CANCELLATION'  
+        elif charge['chargeStatus'] == 'C':
+            charge['chargeStatusString'] = 'FULLY CEASED' 
+
+        if charge['chargeMortgageType'] == 'O':
+            charge['chargeMortgageTypeString'] = 'OPEN TYPE'
+        elif charge['chargeMortgageType'] == 'F':
+            charge['chargeMortgageTypeString'] = 'FOREIGN CURRENCY' 
+        elif charge['chargeMortgageType'] == 'A':
+            charge['chargeMortgageTypeString'] = 'AMOUNT'                
+        elif charge['chargeMortgageType'] == 'M':
+            charge['chargeMortgageTypeString'] = 'MULTIPLE CURRENCIES'   
+
+        charge['chargeAmount'] = float(charge['chargeAmount'])
+        charge['chargeCreateDateString'] = make_aware(datetime.strptime(charge['chargeCreateDate'], '%Y-%m-%dT%H:%M:%S.000Z')).astimezone(pytz.timezone(time_zone)).strftime(date_format)
+        charge['form40DateString'] = make_aware(datetime.strptime(charge['form40Date'], '%Y-%m-%dT%H:%M:%S.000Z')).astimezone(pytz.timezone(time_zone)).strftime(date_format)
+        if 'releaseDate' in charge:
+            charge['releaseDateString'] = make_aware(datetime.strptime(charge['releaseDate'], '%Y-%m-%dT%H:%M:%S.000Z')).astimezone(pytz.timezone(time_zone)).strftime(date_format)
+                      
+        charges_list.append(charge)
+
     data_ready = {
         'mdw1': mdw_1,
-        'mdw2': mdw_2
+        'mdw2': mdw_2,
+        'charges_list': charges_list,
+        'compNoNew': mdw_2['newFormatNo'],
+        'compNoOld': mdw_2['oldFormatNo'],        
     }
 
-    print(data_ready)
 
     return data_ready
--- api-2/products/services/get_info_charges.py
+++ api-2/products/services/get_info_charges.py
@@ -6,32 +6,32 @@ def get_info_charges(url, headers, registration_number, entity_type):
 
    payload = """
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:inf="http://inf.ssm.com.my">
-    <soapenv:Header />
-    <soapenv:Body>
-        <inf:getInfoCharges>
-            <header>
-                <customerId>SSMProduk</customerId>
-                <customerReferenceNo></customerReferenceNo>
-                <customerRequestDate></customerRequestDate>
-            </header>
-            <request>
-                <supplyChargesReq>
-                    <coNo>""" + str(registration_number) + """</coNo>
-                    <gstAmount>0</gstAmount>
-                    <infoAmount>0</infoAmount>
-                    <invoiceNo></invoiceNo>
-                    <ipaddress></ipaddress>
-                    <remark></remark>
-                    <tableId>ROCINFO</tableId>
-                    <type>INFOROCCHRGE</type>
-                </supplyChargesReq>
-            </request>
-        </inf:getInfoCharges>
-    </soapenv:Body>
+   <soapenv:Header/>
+   <soapenv:Body>
+      <inf:getInfoChargeList>
+         <header>
+            <customerId>SSMProduk</customerId>
+            <customerReferenceNo>MBDDTest</customerReferenceNo>
+            <customerRequestDate></customerRequestDate>
+         </header>
+         <request>
+            <supplyChargesReq>
+               <coNo>""" + str(registration_number) + """</coNo>
+               <gstAmount></gstAmount>
+               <infoAmount></infoAmount>
+               <invoiceNo></invoiceNo>
+               <ipaddress></ipaddress>
+               <remark></remark>
+               <tableId>ROCINFO</tableId>
+               <type>INFOROCCHRGE</type>
+            </supplyChargesReq>
+         </request>
+      </inf:getInfoChargeList>
+   </soapenv:Body>
 </soapenv:Envelope>
 """
 
    response = requests.request("POST", url, data=payload, headers=headers)
    response_xml = response.content
    middleware_response_json = json.loads(json.dumps(xmltodict.parse(response_xml)))
-   return middleware_response_json['soapenv:Envelope']['soapenv:Body']['inf:getInfoChargesResponse']['response']['getInfoChargesReturn']
+   return middleware_response_json['soapenv:Envelope']['soapenv:Body']['inf:getInfoChargeListResponse']['response']['getInfoChargeListReturn']
